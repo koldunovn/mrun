63a357e8f46bbdef73c458e8c2c65315566c0258
Nikolay Koldunov <koldunovn@gmail.com>
2015-07-28 13:19:51 +0200

    more

--- remorun.py
+++ remorun.py
@@ -1,13 +1,13 @@
 from jinja2 import Environment, FileSystemLoader
 import os
-import nsub
+import sys
 import time
 import datetime
 import calendar
 from subprocess import Popen, PIPE
 import logging
-from rmodel import diff_month, forcing_present, restart_present, preprocessing, generate_INPUT, postprocessing, check_exitcode, generate_batch_moab, generate_rm_last_mon
-
+from rmodel import * 
+from config import cn
 
 
 PATH = os.path.dirname(os.path.abspath(__file__))
@@ -16,95 +16,53 @@ TEMPLATE_ENVIRONMENT = Environment(
     loader=FileSystemLoader(os.path.join(PATH, 'templates')),
     trim_blocks=False)
 
-logging.basicConfig(filename='log.log', filemode="w", level=logging.DEBUG, format='%(asctime)s %(message)s',datefmt="%Y-%m-%d %H:%M:%S")
+logging.basicConfig(filename='log.log', filemode="w", level=logging.INFO, \
+                    format='%(asctime)s %(message)s',datefmt="%Y-%m-%d %H:%M:%S")
 logging.getLogger().addHandler(logging.StreamHandler())
 
 logging.info('Start script')
 
-#initial date for the whole simmulation
-inidate = datetime.datetime(1960,1,1,0)
+logging.info('First KSA = '+str(cn['KSA']))
+logging.info('First KSE = '+str(cn['KSE']))
 
-#start date for this concrete simulation
-sdate = datetime.datetime(2003,8,1,0)
+cn['firstrun'] = True
 
-#end date for this concrete simulation (not including this date)
-edate = datetime.datetime(2005,1,1,0)
+for i in range(cn['nmonths']):
 
-DT='120'
-
-USER  = '055'
-EXP   = '005'
-BUSER = '055'
-BEXP  = '005' 
-#local place for tar forcing files (will be untared during preprocessing)
-
-MYWRKSHR  = '/lustre/jwork/hhh24/hhh242/GLACINDIA/'
-DIR       = MYWRKSHR+'/'+'tmp_'+USER+EXP
-PFADFRC   = MYWRKSHR+'/FORCING/'
-PFADRES   = MYWRKSHR+'/results/'
-PFL       = '/lustre/jhome15/hhh24/hhh242/sven_remo2008_gletscher_Lake_nk/libs/'
-
-model_exe = 'sven_remo2008'  
-INPUT_file = 'INPUT_'+USER+EXP
-
-xfolders = ['xa', 'xe', 'xf', 'xm', 'xn', 'xt']
-firstrun = True
-
-
-#difference in hours
-KSA_ini = (sdate-inidate).total_seconds()/3600
-#print(str(KSA_ini))
-KSE_ini =  calendar.monthrange(sdate.year,sdate.month)[1]*24
-
-#def diff_month(d1, d2):
-#    return (d1.year - d2.year)*12 + d1.month - d2.month
-
-#number of months
-nmonths = diff_month(edate, sdate)  
-
-#timedelta
-
-KSA = KSA_ini
-KSE = KSA_ini + KSE_ini
-#KSA = 0
-#KSE = 10
-
-date_centered = sdate+datetime.timedelta(14)
-tdiff = KSE_ini
-
-logging.info('First KSA = '+str(KSA))
-logging.info('First KSE = '+str(KSE))
-
-for i in range(nmonths):
-
-    logging.info("\nMonth of the simulation: "+date_centered.strftime('%Y-%m')+'\n')
-    mon_plus = date_centered + datetime.timedelta(tdiff/24)
+    logging.info("\nMonth of the simulation: "+cn['date_present'].strftime('%Y-%m')+'\n')
+    mon_plus  = cn['date_present'] + datetime.timedelta(cn['tdiff']/24)
     
-    forcing_present(PFADFRC ,BUSER, BEXP, date_centered, mon_plus)
-    
-    restart_present(DIR, USER, EXP, date_centered, KSA)
-    
-    preprocessing(MYWRKSHR, PFADFRC, DIR, PFADRES, BUSER, BEXP, date_centered, mon_plus, firstrun, xfolders )
-    
-    generate_INPUT(INPUT_file, KSA, KSE, DT, DIR, MYWRKSHR, USER, EXP, BUSER, BEXP )
+    cn['date_next']    = mon_plus
 
-    time.sleep(10)
+    forcing_present(cn) # checks if 'a' files are in place
+    restart_present(cn) # checks if 'f' and 'g' files are in place
+    preprocessing(cn)   # create directories and untar 'a' files
+    cphclake(cn)        # GLACINDIA specific
+    generate_INPUT(cn)  # generate REMO INPUT file
+    
+    time.sleep(1)
 
-    generate_batch_moab(MYWRKSHR , PFL, model_exe)
-    time.sleep(10)
-    jobid, stout, sterr = nsub.submit_job('moab_remo_sub.sh')
+    generate_batch_slurm(cn) # generate batch script
+    
+    time.sleep(1)
+
+ #   jobid = '12345' # only if we testing
+
+ ############### job submission and monitoring #################   
+    jobid, stout, sterr = submit_job('slurm_remo_sub.sh')
     
     logging.info('Job ID:'+jobid)
 
     complete = False
-    #print(complete)
+
     failedjob = 0
+
     while complete==False:
+        time.sleep(5)
         #print(complete)
         try:
-            a = nsub.get_job_state(int(jobid))
-            logging.info("Job state:"+a['EState'])
-            failedjob = 0
+            jobstate = get_job_state(int(jobid))
+            failedjob = 0 #reset counter of failed attempts to get the job info              
         except:
             if failedjob <= 3:
                 logging.info("Can\'t get information about the job, retrying")
@@ -112,25 +70,30 @@ for i in range(nmonths):
             else:
                 logging.info("No information about the job, give up")
                 raise NameError('No information about the job, give up')
-        complete = nsub.is_job_done(int(jobid))
-        time.sleep(100)
-    #jobid=11  
+        
+        progressbar(cn, jobstate)
+        
+        complete = is_job_done(int(jobid))
     
-    generate_rm_last_mon(DIR, date_centered)
+    print('\n')    
+ ################################################################
 
-    check_exitcode('my-error.txt')
-    postprocessing(MYWRKSHR, PFADFRC, PFADRES, DIR, USER, EXP,  date_centered, jobid) 
-    
+
+    generate_rm_last_mon(cn)
+    final_status(cn, jobid)
+
+    generate_INPUT_press_interp(cn)
+    postprocessing(cn, jobid, execute='slurm', packyear=False, rmyear=False)    
     
     logging.debug("Next month will be: "+mon_plus.strftime('%Y-%m'))
-    tdiff = calendar.monthrange(mon_plus.year,mon_plus.month)[1]*24
-    logging.debug('Number of days in the next month: '+str(tdiff))
-    KSA=KSE
-    logging.debug('New KSA: '+str(KSA))
-    KSE=KSA+tdiff
-    logging.debug('New KSE: '+str(KSE))
+    cn['tdiff'] = calendar.monthrange(mon_plus.year,mon_plus.month)[1]*24
+    logging.debug('Number of days in the next month: '+str(cn['tdiff']))
+    cn['KSA']=cn['KSE']
+    logging.debug('New KSA: '+str(cn['KSA']))
+    cn['KSE']=cn['KSA']+cn['tdiff']
+    logging.debug('New KSE: '+str(cn['KSE']))
 
-    date_centered = mon_plus
+    cn['date_present'] = cn['date_next']
     firstrun=False
 
 os.system('cp log.log ./log/log_{}'.format(datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')))
